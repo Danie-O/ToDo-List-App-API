468b336def3137129d6afd67cb59b718d386b3cc
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-06-30 11:06:42 +0000

    Define environment configs

---
+++ src/config.py
@@ -0,0 +1,23 @@
+import os
+
+
+class Development(object):
+    """ Development environment configurations. """
+    DEBUG = TRUE
+    TESTING = FALSE
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQL_ALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+
+class Production(object):
+    """ Production environment configurations. """
+    DEBUG = FALSE
+    TESTING = FALSE
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQL_ALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+
+app_config = {
+    'development': Development,
+    'production': Production,
+}
