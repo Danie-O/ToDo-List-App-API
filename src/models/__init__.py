ca465da4801367a82c09c7ea7bd0f3e579d7aea4
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-06-30 22:51:18 +0000

    Update __init__ for models

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,1 +1,9 @@
- 
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+# from .TaskModel import TaskModel, TaskSchema
+# from .UserModel import UserModel, UserSchema
+
+#  initialise database
+db = SQLAlchemy()
+bcrypt = Bcrypt()
